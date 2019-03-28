1cbb88d02dbe116ff78559f8eda9bbfbf521c6b8
davlane1 <31543372+davlane1@users.noreply.github.com>
2019-03-28 11:25:11 -0400

    Created data relating to boss

---
+++ UnityProject/Assets/Scripts/bossControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossControl : MonoBehaviour
+{
+    //My ideas are as follows:
+    //The boss does not move from the side of the screen
+    //It fires fireballs and flaming stones at you
+    //Flaming stones will cool down, allowing you to pick them up and throw them at the boss
+    //Touching the boss will hurt you
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
