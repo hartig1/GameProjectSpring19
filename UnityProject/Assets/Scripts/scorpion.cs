98ff5036cba4648bf31c446f4805f8f6b08e6e85
hartig1 <31942356+hartig1@users.noreply.github.com>
2019-05-17 17:50:42 -0400

    Added enemies

--- UnityProject/Assets/Scripts/scorpion.cs
+++ UnityProject/Assets/Scripts/scorpion.cs
@@ -12,6 +12,9 @@ public class scorpion : MonoBehaviour
     private int timer = 0;
     private int health = 2;
     public GameObject scorp;
+    public int dist;
+    private Vector2 start;
+    private Vector2 end;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +22,23 @@ public class scorpion : MonoBehaviour
         //transform.localScale = new Vector2(transform.localScale.x, transform.localScale.y);
         startingScale = transform.localScale.x;
         rb2d.velocity = new Vector2(walkSpeed, 0);
+        start = transform.position;
+        end = transform.position + new Vector3(dist, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(end.x < transform.position.x && dir)
+        {
+            dir = false;
+            rb2d.velocity = -rb2d.velocity;
+        }
+        if(transform.position.x < start.x && !dir)
+        {
+            dir = true;
+            rb2d.velocity = -rb2d.velocity;
+        }
         if(dir)
         {
             //rb2d.AddForce(new Vector2(.35f*walkSpeed,0));
@@ -40,21 +55,6 @@ public class scorpion : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.tag == "wall" && timer == 0)
-        {
-            if (dir)
-            {
-                rb2d.velocity = new Vector2(-walkSpeed, rb2d.velocity[1]);
-                rb2d.position = new Vector2(rb2d.position[0] - 1f, rb2d.position[1]);
-            }
-            else
-            {
-                rb2d.velocity = new Vector2(walkSpeed, rb2d.velocity[1]);
-                rb2d.position = new Vector2(rb2d.position[0] + 1f, rb2d.position[1]);
-            }
-            dir = !dir;
-            timer = 5;
-        }
         if(col.gameObject.tag == "attack")
         {
             health--;
@@ -85,6 +85,10 @@ public class scorpion : MonoBehaviour
                 scorp.SetActive(false);
             }
             timer = 5;
+        }
+        if(col.gameObject.tag == "player")
+        {
+            dir = !dir;
         }
     }
 }
